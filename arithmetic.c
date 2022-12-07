c573367ae491833d25a8ef1170273f5059b7bed3
Apurvaa03 <120012581+Apurvaa03@users.noreply.github.com>
2022-12-07 15:29:29 +0530

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int no1,no2;
+    int res;
+
+    printf("Enter the 1st number :");
+    scanf("%d",&no1);
+    printf("Enter the 2nd number :");
+    scanf("%d",&no2);
+    
+    printf("\nAddition of two nos :%d",no1+no2);
+    printf("\nSubstraction of two nos :%d",no1-no2);
+    printf("\nMultiplication of two nos :%d",no1*no2);
+    printf("\nDivision of two nos :%d",no1/no2);
+
+    return 0;
+}
